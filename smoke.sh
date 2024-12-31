af4ce95b042df506d39585991c76b3d3609fd432
Ankit <patelankitr123@gmail.com>
2024-12-31 16:01:40 +0530

    Report command updated in smoke file

--- smoke.sh
+++ smoke.sh
@@ -3,9 +3,4 @@ echo "Execute tests..."
 pytest -v /var/lib/jenkins/workspace/pythonLearn/Test/ss_test/test_screenshot.py --alluredir=allure-report --take-screenshot=False
 
 echo "Generate Allure report..."
-allure generate -c allure-report -o allure-results-html
-
-echo "Combine Allure report..."
-allure-combine ./allure-results-html
-
-echo "Report generated at: http://208.87.133.122:63342/var/lib/jenkins/workspace/pythonLearn/allure-results-html/index.html"
+allure serve allure-report
