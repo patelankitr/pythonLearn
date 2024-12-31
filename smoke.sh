31a419ef105f9de9e94dbb167ae03ea596f38de9
Ankit <patelankitr123@gmail.com>
2024-12-31 16:10:16 +0530

    Clear report commited

--- smoke.sh
+++ smoke.sh
@@ -1,6 +1,14 @@
 
+echo "Clearing allure-report folder..."
+rm -rf allure-report
+
 echo "Execute tests..."
 pytest -v /var/lib/jenkins/workspace/pythonLearn/Test/ss_test/test_screenshot.py --alluredir=allure-report --take-screenshot=False
 
 echo "Generate Allure report..."
-allure serve allure-report
+allure generate -c allure-report -o allure-results-html
+
+echo "Combine Allure report..."
+allure-combine ./allure-results-html
+
+echo "Report generated at: /var/lib/jenkins/workspace/pythonLearn/allure-results-html/index.html"
