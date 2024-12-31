051bdbfd3a467094ee1baa4f8dadcc746ed398ec
Ankit <patelankitr123@gmail.com>
2024-12-31 17:21:29 +0530

    remove report

--- smoke.sh
+++ smoke.sh
@@ -1,14 +1,4 @@
 
-echo "Clearing allure-report folder..."
-rm -rf allure-report
-
 echo "Execute tests..."
-pytest -v /var/lib/jenkins/workspace/pythonLearn/Test/ss_test/test_screenshot.py --alluredir=allure-report --take-screenshot=False
+pytest -v /var/lib/jenkins/workspace/pythonLearn/Test/ss_test/test_screenshot.py --take-screenshot=False
 
-echo "Generate Allure report..."
-allure generate -c allure-report -o allure-results-html
-
-echo "Combine Allure report..."
-
-
-echo "Report generated at: /var/lib/jenkins/workspace/pythonLearn/allure-results-html/index.html"
